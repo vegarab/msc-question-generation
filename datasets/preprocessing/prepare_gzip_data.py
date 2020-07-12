7fe8b3c4c77d353e71a9ff7c84572366c9107467
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2020-07-12 15:51:40 +0200

    added main function in all executable python scripts

--- datasets/preprocessing/prepare_gzip_data.py
+++ datasets/preprocessing/prepare_gzip_data.py
@@ -3,19 +3,21 @@ import json
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Turn json.gz data to json data.')
-parser.add_argument('--input', type=str,
-                    help='the path to input file')
-parser.add_argument('--output', type=str,
-                    help='the path to output file')
+if __main__ == '__name__':
+    parser = argparse.ArgumentParser(
+        description='Turn json.gz data to json data.')
+    parser.add_argument('--input', type=str,
+                        help='the path to input file')
+    parser.add_argument('--output', type=str,
+                        help='the path to output file')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-input_path = args.input
-output_path = args.output
+    input_path = args.input
+    output_path = args.output
 
-with gzip.open(input_path, 'r') as f:
-    data = json.loads(f.read(), encoding='utf-8')
+    with gzip.open(input_path, 'r') as f:
+        data = json.loads(f.read(), encoding='utf-8')
 
-with open(output_path, 'w') as f:
-    json.dump(data, f)
+    with open(output_path, 'w') as f:
+        json.dump(data, f)
--- datasets/preprocessing/squad_to_csv.py
+++ datasets/preprocessing/squad_to_csv.py
@@ -6,7 +6,8 @@ import numpy as np
 
 
 def squad_json_to_dataframe(input_file_path,
-                            record_path=['data','paragraphs','qas','answers'],
+                            record_path=[
+                                'data', 'paragraphs', 'qas', 'answers'],
                             verbose=1):
     """
     input_file_path: path to the squad json file.
@@ -15,21 +16,22 @@ def squad_json_to_dataframe(input_file_path,
     verbose: 0 to suppress it default is 1
     """
     if verbose:
-        print("Reading the json file")    
+        print("Reading the json file")
     file = json.loads(open(input_file_path).read())
     if verbose:
         print("processing...")
     # parsing different level's in the json file
-    js = pd.io.json.json_normalize(file , record_path )
-    m = pd.io.json.json_normalize(file, record_path[:-1] )
-    r = pd.io.json.json_normalize(file,record_path[:-2])
-    
-    #combining it into single dataframe
+    js = pd.io.json.json_normalize(file, record_path)
+    m = pd.io.json.json_normalize(file, record_path[:-1])
+    r = pd.io.json.json_normalize(file, record_path[:-2])
+
+    # combining it into single dataframe
     idx = np.repeat(r['context'].values, r.qas.str.len())
-    ndx  = np.repeat(m['id'].values,m['answers'].str.len())
+    ndx = np.repeat(m['id'].values, m['answers'].str.len())
     m['context'] = idx
     js['q_idx'] = ndx
-    main = pd.concat([ m[['id','question','context','answers']].set_index('id'),js.set_index('q_idx')],1,sort=False).reset_index()
+    main = pd.concat([m[['id', 'question', 'context', 'answers']].set_index(
+        'id'), js.set_index('q_idx')], 1, sort=False).reset_index()
     main['c_id'] = main['context'].factorize()[0]
     if verbose:
         print("shape of the dataframe is {}".format(main.shape))
@@ -37,22 +39,23 @@ def squad_json_to_dataframe(input_file_path,
     return main
 
 
-parser = argparse.ArgumentParser(description='Turn SQuAD JSON to flat CSV')
-parser.add_argument('--input', type=str,
-                    help='the path to input file')
-parser.add_argument('--output', type=str,
-                    help='the path to output file')
+def __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Turn SQuAD JSON to flat CSV')
+    parser.add_argument('--input', type=str,
+                        help='the path to input file')
+    parser.add_argument('--output', type=str,
+                        help='the path to output file')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-input_path = args.input
-output_path = args.output
+    input_path = args.input
+    output_path = args.output
 
-dataframe = squad_json_to_dataframe(input_path)
+    dataframe = squad_json_to_dataframe(input_path)
 
-# Only use necessary columns and set appropriate names
-dataframe = dataframe[['index', 'question', 'context', 'text', 'answer_start']]
-dataframe.rename(columns={'text':'answer'}, inplace=True)
+    # Only use necessary columns and set appropriate names
+    dataframe = dataframe[['index', 'question',
+                           'context', 'text', 'answer_start']]
+    dataframe.rename(columns={'text': 'answer'}, inplace=True)
 
-dataframe.to_csv(output_path)
-
+    dataframe.to_csv(output_path)
