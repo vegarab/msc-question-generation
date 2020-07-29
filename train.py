465578ac853d2086f714733d8b4d3a377701197d
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2020-07-29 21:59:31 +0200

    Revert "load from checkpoint functionality"
    This reverts commit 26bf482183554fdbbf221453fd0edd2bcefc5646.

--- train.py
+++ train.py
@@ -38,22 +38,9 @@ def run(args=None):
     os.environ["WANDB_PROJECT"] = args.wandb_project
     logging.getLogger('wandb.run_manager').setLevel(logging.WARNING)
 
-    # TODO: is this even needed if we just make --model_name the path whenever
-    # --use_checkpoint is set?
-    # Load model, either base or saved checkpoint
-    if args.use_checkpoint:
-        if args.model_path is None:
-            raise ValueError("Missing model path when --use_checkpoint is set")
-
-        model = AutoModelForSeq2SeqLM.from_pretrained(args.model_path)
-    else:
-        model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name)
-
-    # Load tokenizer, none of these are trained further and therefore no need
-    # to be able to load from checkpoint
     tokenizer_name = args.model_name if args.tokenizer_name is None else args.tokenizer_name
-    tokenizer = MODEL_TO_TOK[tokenizer_name].from_pretrained(
-        tokenizer_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name)
+    tokenizer = MODEL_TO_TOK[tokenizer_name].from_pretrained(tokenizer_name)
 
     train_data = torch.load(
         args.train_data_path) if training_args.do_train else None
@@ -69,7 +56,7 @@ def run(args=None):
                       train_dataset=train_data,
                       eval_dataset=test_data,
                       data_collator=collator,
-                      prediction_loss_only=False)
+                      prediction_loss_only=True)
 
     if training_args.do_train:
         trainer.train(model_path=args.model_path)
