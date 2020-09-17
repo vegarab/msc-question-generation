40b993438b694455915cf6a79a55e41a59f47447
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2020-09-17 14:37:19 +0200

    made scipts executable and fixed typo

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p models
-python trian.py --model_name t5-base --dataset squad --output_dir models/squad_t5_100  --data_size 100
+python train.py --model_name t5-base --dataset squad --output_dir models/squad_t5_100  --data_size 100
